d7c86ce91020d7c765ff996b4e2896dead427600
mateustrindade0 <mateus.trindade@dvi.net.br>
2025-11-12 20:16:03 -0300

    Criado o método de formatar os dados no terminal

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,15 +1,37 @@
-public class Cliente { 
+package models;
+
+public class Cliente {
+    
+    // Attributes
+    private int id;
     private String nome;
     private String cpf;
     private String telefone;
 
-
-    public Cliente(String nome, String cpf, String telefone){
+    // Constructor
+    public Cliente(String nome, String cpf, String telefone) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
     }
 
+    // Getters and Setters
+    public int getId(){return id;}
+    public void setId(int id) { this.id = id; }
+
+    public String getNome(){return nome;}
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() {return cpf;}
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getTelefone() {return telefone;}
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nome='" + nome + '\'' + ", cpf='" + cpf + '\'' + ", telefone='" + telefone + '\'' + '}';
+    }
 
 
 }
