9169eea8739f8ef60e6368e5b5e2a19763639cd3
mateustrindade0 <mateus.trindade@dvi.net.br>
2025-11-11 21:24:55 -0300

    Criado o objeto Cliente

---
+++ src/models/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente { 
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+
+    public Cliente(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+
+
+}
