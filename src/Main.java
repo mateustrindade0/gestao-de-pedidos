b9e6f3f09c9947ab79508385530f7414fb1b9e4f
iurygabriel460-gif <iurygabriel460@gmail.com>
2025-11-11 22:28:57 -0300

    teste de conecao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("🚗 Sistema de Locadora  iniciado via terminal...");
+        System.out.println("🚗 Sistema de Locadora  via terminal...");
     }
 }
